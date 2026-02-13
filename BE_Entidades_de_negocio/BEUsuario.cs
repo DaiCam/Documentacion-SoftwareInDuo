8bf1657188d89235a42f2dab754e6d561f38ca10
DaiCam <dcampott@gmail.com>
2026-02-13 20:40:58 -0300

    Versión inicial

---
+++ BE_Entidades_de_negocio/BEUsuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_Entidades_de_negocio
+{
+    internal class BEUsuario
+    {
+        //PROPIEDADES
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public Boolean Activo { get; set; }
+
+        //CONSTRUCTOR
+        public BEUsuario() { }
+
+        //CONSTRUCTOR CON PARAMETROS 
+        public BEUsuario(int idUsuario, string nombre, string apellido, string email, string contraseña, bool activo)
+        {
+            IdUsuario = idUsuario;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Contraseña = contraseña;
+            Activo = activo;
+        }
+    }
+
+}
